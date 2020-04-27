b010156550a340878463f24ac4628cdd5d301060
Danielli.Itai <Danielli.Itai@gmail.com>
2020-04-27 15:35:40 +0300

    Adding code outline and documentation for C1M1 assesment

--- stats.c
+++ stats.c
@@ -9,13 +9,14 @@
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file stats.c
+ * @brief basic statistical analysis for characters array.
  *
- * <Add Extended Description Here>
+ * 1. Aanalsis of an array of unsigned char data items.
+ * 2. Reporting analytics on the maximum, minimum, mean, and median of the data set.
  *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author Itai Danielli
+ * @date 27/04/2020
  *
  */
 
@@ -24,11 +25,56 @@
 #include <stdio.h>
 #include "stats.h"
 
+
+
 /* Size of the Data Set */
 #define SIZE (40)
 
-void main() {
+/***
+*/
+void print_statistics(unsigned char min, unsigned char max, unsigned char mean, unsigned char median)
+{
+}
 
+void print_array(unsigned char* p_test, unsigned int len)
+{
+}
+
+unsigned char find_minimum(unsigned char* p_test, unsigned int len)
+{
+	unsigned char minimum = 0xFF;
+	return(minimum);
+}
+
+unsigned char find_maximum(unsigned char* p_test, unsigned int len)
+{
+	unsigned char maximum = 0x00;
+	return(maximum);
+}
+
+unsigned char find_mean(unsigned char* p_test, unsigned int len)
+{
+	unsigned int mean= 0x00;
+	return(mean);
+
+}
+
+void sort_array(unsigned char* p_test, unsigned int len)
+{
+	return;
+}
+
+unsigned char find_median(unsigned char* p_test, unsigned int len)
+{
+	unsigned char median = 0x00;
+	return(median);
+}
+
+
+
+void main()
+ {
+  int size = SIZE;
   unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
                               114, 88,   45,  76, 123,  87,  25,  23,
                               200, 122, 150, 90,   92,  87, 177, 244,
@@ -36,8 +82,16 @@ void main() {
                                 7,  87, 250, 230,  99,   3, 100,  90};
 
   /* Other Variable Declarations Go Here */
+  unsigned char minimum = find_minimum(test, size);
+  unsigned char maximum = find_maximum(test, size);
+  unsigned char mean = find_mean(test, size);
+  unsigned char median = find_median(test, size);
+  
   /* Statistics and Printing Functions Go Here */
-
+  print_statistics(minimum, maximum, mean, median);
+  print_array(test, size);
+  
+  return;
 }
 
 /* Add other Implementation File Code Here */
--- stats.h
+++ stats.h
@@ -23,18 +23,101 @@
 
 /* Add Your Declarations and Function Comments here */ 
 
-/**
- * @brief <Add Brief Description of Function Here>
- *
- * <Add Extended Description Here>
- *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- *
- * @return <Add Return Informaiton here>
- */
+  /**
+   * Function: find_average
+   * @brief print the statistical analisys results.
+   *
+   * This function prints out the statistical analysis results.
+   *    mininmum value.
+   *
+   * @param min minimum number.
+   *
+   * @return No return value.
+   */
+  void print_statistics(unsigned char min, unsigned char max, unsigned char mean, unsigned char median);
 
+  /**
+   * Function: find_average
+   * @brief print the array elements.
+   *
+   * This function prints prints the array to the screen
+   * Given an array of data and a length.
+   *
+   * @param p_test pointer to the array.
+   * @param len array size.
+   *
+   * @return No return value.
+   */
+  void print_array(unsigned char* p_test, unsigned int len);
 
+  /**
+   * Function: find_minimum
+   * @brief finds the minimum value in an array.
+   *
+   * This function returns the minimum value of an array.
+   * The function returns 0xFF In case of an error or no value can be found.
+   *
+   * @param p_test pointer to the array.
+   * @param len array size.
+   *
+   * @return the array minimum value.
+   */
+  unsigned char find_minimum(unsigned char* p_test, unsigned int len);
+
+  /**
+   * Function: find_maximum
+   * @brief finds the maximum value in an array.
+   *
+   * This function returns the maximum value in an array.
+   * The function returns 0x00 In case of an error or no value can be found.
+   *
+   * @param p_test pointer to the array.
+   * @param len array size.
+   *
+   * @return the array maximum value.
+   */
+  unsigned char find_maximum(unsigned char* p_test, unsigned int len);
+
+  /**
+   * Function: find_mean
+   * @brief calculates the mean value of an array.
+   *
+   * This function returns the mean of an array set of valus.
+   * The function returns 0x00 In case of error or an array size 0.
+   *
+   * @param p_test pointer to the array.
+   * @param len array size.
+   *
+   * @return the array mean value.
+   */
+  unsigned char find_mean(unsigned char* p_test, unsigned int len);
+
+  /**
+   * Function: sort_array
+   * @brief sorts an array in place.
+   *
+   * This function sorts an array in dacceseeding order from max in place 0.
+   * to minimum in the last place.
+   *
+   * @param p_test pointer to the array.
+   * @param len array size.
+   *
+   * @return No return value.
+   */
+  void sort_array(unsigned char* p_test, unsigned int len);
+
+  /**
+   * Function: find_median
+   * @brief retrurns the median given a sorted array.
+   *
+   * This function calculates the median of a sorted array.
+   * In case of even length the median is the avarage of the two valuaes at
+   * len/2 and len/2 + 1
+   *
+   * @param p_test pointer to the array.
+   * @param len array size.
+   *
+   * @return median value.
+   */
+  unsigned char find_median(unsigned char* p_test, unsigned int len);
 #endif /* __STATS_H__ */
